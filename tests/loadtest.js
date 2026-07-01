7dd8f46f0e15ed5b1c607d1dabbfe869d8afb5a6
Nikolay Kashev <nikolay.kashev@gmail.com>
2026-07-01 15:48:47 +0300

    Modify load test target from 400 to 300
    Updated load test stages to change target from 400 to 300.

--- tests/loadtest.js
+++ tests/loadtest.js
@@ -22,7 +22,7 @@ export const options = {
   //   { duration: '2m', target: 50 },
   //   { duration: '2m', target: 100 },
   //   { duration: '2m', target: 200 },
-  //   { duration: '2m', target: 400 },
+  //   { duration: '2m', target: 300 },
   //   { duration: '2m', target: 0 },
   // ],
 
